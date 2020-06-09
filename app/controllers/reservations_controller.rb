0316faf8b066b0a91f75f7f6f3dddadd9f480708
Richard Mendy <mendyrichard@outlook.com>
2020-06-09 15:56:08 +0200

    reservations routes ok

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,25 @@
 class ReservationsController < ApplicationController
+  before_action :set_bar, only: [:new, :create]
+  
   def new
     @reservation = Reservation.new
   end
 
   def create
     @reservation = Reservation.new(reservation_params)
-    @reservation.save
+    @reservation.bar = @bar
+    if @reservation.save
+      redirect_to root_path # redirect to dashboard!!!!
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_bar
+    @bar = Bar.find(params[:bar_id])
+  end
 
   def reservation_params
     params.require(:reservation).permit(:user_id, :bar_id, :date)
