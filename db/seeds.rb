aa5d5abb085f57ef00ae1adadd1ded5567c0d13b
Mael Harnois <mael.hrn@gmail.com>
2020-06-09 18:22:02 +0200

    seed update - NOT WORKING YET - DO NOT MERGE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts ".... SEEDS STARTING"
 User.destroy_all
 Review.destroy_all
 Bar.destroy_all
 Reservation.destroy_all
 
-u1 = User.create!(first_name: "Thibault", last_name: "le beau gosse", email: "edeecvece@eddfze.com", password:"123456")
+u1 = User.create!(first_name: "Thibault", last_name: "le beau gosse", email: "u1@wagon.com", password:"secret")
 
-u2 = User.create!(first_name: "Mael", last_name: "Le prince", email: "edeecerce@eddfze.com", password:"1234567")
+u2 = User.create!(first_name: "Mael", last_name: "Le prince", email: "u2@wagon.com", password:"secret")
 
-u3 = User.create!(first_name: "Richard", last_name: "La belle vie", email: "edeecece@eddfzze.com", password:"123456")
+u3 = User.create!(first_name: "Richard", last_name: "La belle vie", email: "u3@wagon.com", password:"secret")
 
 b1 = Bar.create!(name:"Le Wagon", capacity: 5, ambiance: "hot", address: "rue des capucins", city: "lyon", owner_id: u1.id)
+
+mood = %w[electro punk reggae hip-hop jazz]
+villes = %w[lyon montpellier paris nantes]
+10.times do |i|
+  Bar.create!(name:"bar#{i}", capacity: i*2, ambiance: mood.sample, address: "#{i} rue des Capucins", city: villes.sample, owner_id: u1.id)
+end
 
 b1.owner_id = u1.id
 
 res1 = Reservation.create!(date: Date.today, user_id: u2.id, bar_id: b1.id)
 
 rev1 = Review.create!(user_id: u2.id, bar_id: b1.id, rating: 4, content: "this is a beautifull day")
+
+puts ".... SEEDS OK"
