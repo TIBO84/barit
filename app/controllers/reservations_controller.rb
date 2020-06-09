f5eaa02191902c0ae5eb9de9145fb3f603d39dd3
Richard Mendy <mendyrichard@outlook.com>
2020-06-09 14:22:50 +0200

    controller reservation et views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,16 @@
 class ReservationsController < ApplicationController
   def new
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.save
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :bar_id, :date)
   end
 end
