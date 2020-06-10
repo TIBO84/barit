be707342689b9dda1ad63e337d1a2c63abd0c659
Mael Harnois <mael.hrn@gmail.com>
2020-06-10 18:13:41 +0200

    update dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,37 @@
 module ApplicationHelper
-
   def back_button_path(search)
     if search.blank?
       root_path
     else
       bars_path(city: search)
+    end
+  end
+
+  def owner?(user)
+    user.bars.any?
+  end
+
+  def renter?(user)
+    user.reservations.any?
+  end
+
+  def address(bar)
+    return "#{bar.address}, #{bar.city}"
+  end
+
+  def total_reservations(bar)
+    x = 0
+    bar.reservations.each do |r|
+      x +=1
+    end
+    return x
+  end
+
+  def next_reservation(bar)
+    if bar.reservations.any?
+      bar.reservations.sort{ |a,b| a.date <=> b.date }.first.date
+    else
+      return "--"
     end
   end
 end
