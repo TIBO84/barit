eeda34f16384ecc8d5c12366e83301edc19b8dd8
Mael Harnois <mael.hrn@gmail.com>
2020-06-11 17:40:46 +0200

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -98,7 +98,7 @@ o2 = User.create!(first_name: "Mael", last_name: "Efant", email: "o2@wagon.com",
 u1 = User.create!(first_name: "Richard", last_name: "Dassault", email: "u1@wagon.com", password:"secret")
 u2 = User.create!(first_name: "Jo", last_name: "Zéfine", email: "u2@wagon.com", password:"secret")
 u3 = User.create!(first_name: "Cassandre", last_name: "Ié", email: "u3@wagon.com", password:"secret")
-mood = %w[electro punk reggae hip-hop jazz]
+mood = %w[Electro Punk Reggae Hip-hop Jazz Hot]
 villes = %w[lyon montpellier nantes]
 
 owners = [o1, o2]
